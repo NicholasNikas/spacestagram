e40818ba61f786fa253f604c3e87366aab330f23
Nicholas Nikas <nicholasnikas@Nicholass-MacBook-Pro.local>
2022-01-18 17:23:13 -0500

    Add API key and axios request to fetch Nasa APOD

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,7 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Header from './Header'
+import axios from 'axios'
+
+const nasaApiKey = process.env.REACT_APP_NASA_API_KEY
 
 const Gallery = () => {
+  const [galleryData, setGalleryData] = useState(null)
+
+  const fetchNasaApodPhotos = async () => {
+    // CHANGE TO AXIOS
+    // const response = await fetch(
+    //   `https://api.nasa.gov/planetary/apod?api_key=${nasaApiKey}&count=10`
+    // )
+
+    const { data } = await axios.get(
+      `https://api.nasa.gov/planetary/apod?api_key=${nasaApiKey}&count=10`
+    )
+    setGalleryData(data)
+    console.log(data)
+  }
+
+  useEffect(() => {
+    fetchNasaApodPhotos()
+  }, [])
+
   return (
     <div className='gallery'>
       <Header />
