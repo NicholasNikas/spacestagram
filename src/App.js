d487f98df44ca06b5f4a54e1679c2c3d9443bad7
Nicholas Nikas <nicholasnikas@Nicholass-MacBook-Pro.local>
2022-01-19 01:20:38 -0500

    Delete useless files from create-react-app and edit favicon.ico

--- src/App.js
+++ src/App.js
@@ -6,14 +6,12 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
-    <div>
       <Router>
         <Routes>
           <Route element={<HomePage />} path='/' exact />
           <Route element={<Gallery />} path='/gallery' />
         </Routes>
       </Router>
-    </div>
   )
 }
 
